78aad6357188eca2ddbbe9e09435f3a89f1f197c
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-12-09 21:37:29 -0800

    added category service

---
+++ manage-category-service/src/main/java/com/category/bean/CategoryBean.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.category.bean;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author nsanda
+ *
+ */
+@Setter
+@Getter
+public class CategoryBean implements Serializable {
+
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7032193364160823070L;
+
+	private int id;
+	private String catName;
+	private String catImage;
+	private String status;
+	private boolean isOffer;
+	
+
+}
